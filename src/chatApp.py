73a687b71f4b0b5d5b4f90a0167acfb6b83d061d
GilaMiriamKuber <gilamkuber@gamil.com>
2023-08-24 16:33:01 +0300

    room directory wokes!

--- src/chatApp.py
+++ src/chatApp.py
@@ -1,23 +1,18 @@
 from flask import Flask, redirect, request, render_template, session
 import csv
 import os
-# from dotenv import load_dotenv
-# load_dotenv()
+
 app = Flask(__name__)
 app.secret_key = "tamar_gilamiriam_!_@_?_chat_app"
-app.config["SECRET_KEY"]="tamar_gilamiriam_!_@_?_chat_app"
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 
-
-server = Flask(__name__ , template_folder="templates")
-
-@server.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def toHomePage():
     return redirect("/register")
 
-@server.route("/register", methods=['GET', 'POST'])
-def homePage():
+@app.route("/register", methods=['GET', 'POST'])
+def register():
     if request.method == 'POST':
         name = request.form["username"]
         password = request.form["password"]
@@ -25,9 +20,8 @@ def homePage():
         return redirect('/login')
     return render_template('register.html')
 
-
-@server.route("/login", methods=['GET', 'POST'])
-def loginPage():
+@app.route("/login", methods=['GET', 'POST'])
+def login():
     if request.method == 'POST':
         name = request.form["username"]
         password = request.form["password"]
@@ -37,59 +31,52 @@ def loginPage():
             return redirect('/lobby')
     return render_template('login.html')
 
-
-@server.route("/logout", methods=['GET', 'POST'])
+@app.route("/logout", methods=['GET', 'POST'])
 def logOut():
     session.pop('username', None)
     return redirect("/login")
-    
-@server.route("/chat/<room>", methods=['GET', 'POST'])
+
+@app.route("/chat/<room>", methods=['GET', 'POST'])
 def chatPage(room):
     return render_template('chat.html')
 
-@server.route("/lobby", methods=['GET', 'POST'])
+@app.route("/lobby", methods=['GET', 'POST'])
 def lobby():
-    if session.get('usernme') == True:
-            if request.method == 'POST':
-                new_room = request.form['new_room']
-                # path = os.getenv('ROOMS_PATH') + new_room + ".txt"
-                path = './src/rooms' + new_room + ".txt"
-                open(path , 'w')
-                rooms=  os.listdir('./src/rooms')
-                new_rooms = [x[:-4] for x in rooms]
-                return render_template('lobby.html',room_names=new_rooms)
+    if 'username' in session:
+        if request.method == 'POST':
+            room_name = request.form['new_room']
+            path='./rooms/'+room_name+".txt"
+            open(path, 'w')  
+        rooms = os.listdir('./rooms')
+        new_rooms = [x[:-4] for x in rooms]
+        print(new_rooms)
+        return render_template('lobby.html', room_names = new_rooms)
     else:
-        return render_template('login.html') 
+        return redirect('/login')
 
-
-@server.route("/chat/<int:room> ", methods=['GET', 'POST'])
+@app.route("/chat/<int:room> ", methods=['GET', 'POST'])
 def chatRoom():
     return render_template('chat.html')
 
 def saveInCsv(name, password):
     with open('users.csv', 'rt') as f:
-        reader = csv.reader(f, delimiter=',') 
+        reader = csv.reader(f, delimiter=',')
         for row in reader:
             if name == row[0]:
                 f.close()
                 return True
     with open('users.csv', 'a') as f:
         writer = csv.writer(f)
-        #encPassword = password.encode()
-        writer.writerow([name,password])
-        f.close()
-        return False
+        writer.writerow([name, password])
+    return False
 
 def checkIfExist(name, password):
     with open('users.csv', 'rt') as f:
-        reader = csv.reader(f, delimiter=',') 
+        reader = csv.reader(f, delimiter=',')
         for row in reader:
-            if name == row[0]:
-                if password == row[1]:
-                    f.close()
-                    return True
+            if name == row[0] and password == row[1]:
+                return True
         return False
 
-
 if __name__ == "__main__":
-    server.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
